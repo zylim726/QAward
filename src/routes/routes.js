c055df7f8ecb934334d753ade5b735e914b36a4d
zylim726 <156066407+zylim726@users.noreply.github.com>
2024-07-04 15:43:18 +0800

    update router js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -27,7 +27,7 @@ const routes = [
     children: [
       {
         path: "projectlist",
-        name: "Project List789",
+        name: "Project List",
         component: ProjectList,
         meta: { requiresAuth: true },
       },
