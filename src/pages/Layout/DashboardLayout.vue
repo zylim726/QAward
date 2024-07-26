ae10cc3d1973c9b8ec8f11ca892187914c455bb3
zylim726 <156066407+zylim726@users.noreply.github.com>
2024-07-26 13:52:59 +0800

    Update Dashboard layout

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -11,6 +11,10 @@
         <md-icon>check_circle_outline</md-icon>
         <p class="nav-item">Comparison Summary</p>
       </sidebar-link>
+      <sidebar-link class="navbar-link" to="/laWoSummaryList" style="">
+        <md-icon>data_exploration</md-icon>
+        <p class="nav-item">LA / WO Summary</p>
+      </sidebar-link><br>
 
       <h4 class="menutitle">Master</h4>
       <sidebar-link class="navbar-link" to="/projectsetup">
