5dd98a6edf1300a6767e118258d49592192a4757
zylim726 <156066407+zylim726@users.noreply.github.com>
2024-08-01 09:04:51 +0800

    Update store.js

--- src/services/axios/store.js
+++ src/services/axios/store.js
@@ -25,6 +25,12 @@ export default new Vuex.Store({
       localStorage.removeItem("token");
       localStorage.removeItem("accesslevel");
       localStorage.removeItem("username");
+<<<<<<< Updated upstream
+=======
+      localStorage.removeItem("userid");
+      localStorage.removeItem("projectId");
+      localStorage.removeItem("projectName");
+>>>>>>> Stashed changes
     },
   },
   actions: {
