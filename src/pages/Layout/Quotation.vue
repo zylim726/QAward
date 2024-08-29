9ba930e00582a0c75776c81ade1bf9593b20b229
zylim726 <156066407+zylim726@users.noreply.github.com>
2024-08-29 18:35:57 +0800

    Update Quotation.vue

--- src/pages/Layout/Quotation.vue
+++ src/pages/Layout/Quotation.vue
@@ -393,17 +393,17 @@ export default {
             }
             if (QuotationData.rateData === QuotationData.countData && QuotationData.rateData != 0) {
               const SuccessMessage = await QuotationController.addQuotation(QuotationData,SubConName,Discount,Remarks,Documents,id);
-              console.log('successMessage',SuccessMessage);
-              // const concatenatedMessage = SuccessMessage.join(', ');
-              // this.UpdateMessage = concatenatedMessage.split(',')[0].trim();
-              // window.scrollTo(0, 0); 
-              // const storedProjectId = localStorage.getItem('projectId');
+           
+              const concatenatedMessage = SuccessMessage.join(', ');
+              this.UpdateMessage = concatenatedMessage.split(',')[0].trim();
+              window.scrollTo(0, 0); 
+              const storedProjectId = localStorage.getItem('projectId');
              
-              // this.$router.push({
-              //     name: 'Subcon Comparison',
-              //     query: { cqID: this.$route.query.cqId, 
-              //       projectID: storedProjectId }
-              // });
+              this.$router.push({
+                  name: 'Subcon Comparison',
+                  query: { cqID: this.$route.query.cqId, 
+                    projectID: storedProjectId }
+              });
 
              
             } else {
