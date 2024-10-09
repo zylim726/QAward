a7c16a114bd6e1b52e40b7e3772d9951a9f5ee22
Lim Zi Yu <zylim@metrio.local>
2024-10-09 14:02:45 +0800

    enhancement/display-approver
    1. Enhance the approver design for better responsiveness on desktop-sized screens.

--- src/components/Tables/ComparisonTable.vue
+++ src/components/Tables/ComparisonTable.vue
@@ -153,7 +153,7 @@
           <template><br>
             <div class="container" style="width: 100%;">
               <ApprovalTable v-if="cmCQapproval.length > 0" :cmCQapproval="cmCQapproval" />
-              <div class="row" v-for="(approvalData, index) in filteredCQApprovalData" :key="index" style=" margin-top: 15px; margin-right: 20px;">
+              <div class="row" v-for="(approvalData, index) in filteredCQApprovalData" :key="index" style="width: 60%; margin-top: 15px; margin-right: 20px;align-items: start;">
                 <div class="cqbox">
                   <div class="left-container">
                     <div class="md-card-avatar" style="margin-bottom: 160px;">
